5369baa447f60e3c9029c2fbadf0ee6c2441867d
jst4rk <dayron.alfa@gmail.com>
2022-11-03 15:22:46 -0400

    feat: basic game and player class code

--- game.js
+++ game.js
@@ -5,5 +5,73 @@ window.addEventListener('load', function () {
 
     canvas.width = 1500;
     canvas.height = 500;
-    
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+        constructor(game) {
+            this.game = game;
+
+            this.width = 120;
+            this.height = 190;
+
+            this.xPosition = 20;
+            this.yPosition = 100;
+
+            this.ySpeed = 0;
+        }
+
+        update() {
+            this.yPosition += this.ySpeed;
+        }
+
+        draw(ctx) {
+            ctx.fillRect(this.xPosition, this.yPosition, this.width, this.height);
+        }
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+
+    }
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+
+            this.player = new Player(this);
+        }
+
+        update() {
+            this.player.update();
+        }
+
+        draw(ctx) {
+            this.player.draw(ctx);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
 });
